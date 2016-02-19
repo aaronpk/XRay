966eaa1b60d71c4cf62ff186ed4b75aa0a83d4e5
Aaron Parecki <aaron@parecki.com>
2016-02-19 09:54:00 -0800

    add semantic-ui and jquery

--- views/layout.php
+++ views/layout.php
@@ -3,6 +3,9 @@
 <head>
   <title><?= $this->e($title) ?></title>
   <link href="/assets/style.css" rel="stylesheet">
+  <link href="/semantic-ui/semantic.min.css" rel="stylesheet">
+  <script src="/assets/jquery-1.11.3.min.js"></script>
+  <script src="/semantic-ui/semantic.min.js"></script>
   <script src="/assets/script.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
