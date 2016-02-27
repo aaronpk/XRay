ed88b4881bec7ef2ad4ddaefb96fd7c781ef222b
Aaron Parecki <aaron@parecki.com>
2016-02-27 15:05:45 -0800

    use file_get_contents only for appengine URLs

--- lib/HTTP.php
+++ lib/HTTP.php
@@ -1,6 +1,41 @@
 <?php
 namespace p3k;
 
-class HTTP extends HTTPStream {
-}
+class HTTP {
 
+  public $timeout = 4;
+  public $max_redirects = 8;
+
+  public function get($url) {
+    $class = $this->_class($url);
+    $http = new $class($url);
+    $http->timeout = $this->timeout;
+    $http->max_redirects = $this->max_redirects;
+    return $http->get($url);
+  }
+
+  public function post($url, $body, $headers=array()) {
+    $class = $this->_class($url);
+    $http = new $class($url);
+    $http->timeout = $this->timeout;
+    $http->max_redirects = $this->max_redirects;
+    return $http->post($url, $body, $headers);
+  }
+
+  public function head($url) {
+    $class = $this->_class($url);
+    $http = new $class($url);
+    $http->timeout = $this->timeout;
+    $http->max_redirects = $this->max_redirects;
+    return $http->head($url);
+  }
+
+  private function _class($url) {
+    if(preg_match('/brid\.gy|appspot\.com/', $url)) {
+      return 'p3k\HTTPStream';
+    } else {
+      return 'p3k\HTTPCurl';
+    }
+  }
+
+}
--- tests/FetchTest.php
+++ tests/FetchTest.php
@@ -10,19 +10,19 @@ class FetchTest extends PHPUnit_Framework_TestCase {
     $this->http = new p3k\HTTP();
   }
 
-  // public function testTimeout() {
-  //   $url = 'https://nghttp2.org/httpbin/delay/2';
-  //   $this->http->timeout = 1;
-  //   $response = $this->http->get($url);
-  //   $this->assertEquals('timeout', $response['error']);
-  // }
+  public function testTimeout() {
+    $url = 'https://nghttp2.org/httpbin/delay/2';
+    $this->http->timeout = 1;
+    $response = $this->http->get($url);
+    $this->assertEquals('timeout', $response['error']);
+  }
 
-  // public function testRedirectLimit() {
-  //   $url = 'https://nghttp2.org/httpbin/redirect/3';
-  //   $this->http->max_redirects = 1;
-  //   $response = $this->http->get($url);
-  //   $this->assertEquals('too_many_redirects', $response['error']);
-  // }
+  public function testRedirectLimit() {
+    $url = 'https://nghttp2.org/httpbin/redirect/3';
+    $this->http->max_redirects = 1;
+    $response = $this->http->get($url);
+    $this->assertEquals('too_many_redirects', $response['error']);
+  }
 
   public function testNoError() {
     $url = 'https://nghttp2.org/httpbin/ip';
