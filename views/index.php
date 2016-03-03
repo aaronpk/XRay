3d29776ae7d1361c8e8f7378670e9962ab81d425
Aaron Parecki <aaron@parecki.com>
2016-03-02 16:54:23 -0800

    add a form to the home page to allow passing in HTML

--- views/index.php
+++ views/index.php
@@ -5,17 +5,37 @@
 
     <h1>X-Ray</h1>
 
-    <form class="ui large form" action="/parse" method="get">
-      <div class="ui stacked segment">
-        <div class="field">
-          <div class="ui left icon input">
-            <i class="linkify icon"></i>
-            <input type="url" name="url" placeholder="http://example.com">
+    <div class="ui top attached tabular menu">
+      <a class="item active" data-tab="url">URL</a>
+      <a class="item" data-tab="html">HTML</a>
+    </div>
+    <div class="ui bottom attached tab segment active" data-tab="url">
+      <form class="ui large form" action="/parse" method="get">
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="linkify icon"></i>
+              <input type="url" name="url" placeholder="http://example.com">
+            </div>
           </div>
-        </div>
-        <button class="ui fluid large teal submit button">Go</button>
-      </div>
-    </form>
+          <button class="ui fluid large teal submit button">Go</button>
+      </form>
+    </div>
+    <div class="ui bottom attached tab segment" data-tab="html">
+      <form class="ui large form" action="/parse" method="post">
+          <div class="field">
+            <div class="ui left icon input">
+              <textarea name="html" placeholder="HTML"></textarea>
+            </div>
+          </div>
+          <div class="field">
+            <div class="ui left icon input">
+              <i class="linkify icon"></i>
+              <input type="url" name="url" placeholder="http://example.com">
+            </div>
+          </div>
+          <button class="ui fluid large teal submit button">Go</button>
+      </form>
+    </div>
 
     <p><a href="https://github.com/aaronpk/XRay">Read Me</a>. Please <a href="https://github.com/aaronpk/XRay/issues">file an issue</a> if you encounter any issues.</p>
 
@@ -37,3 +57,9 @@
     max-width: 450px;
   }
 </style>
+<script>
+$(function(){
+  $('.menu .item').tab();
+;
+});
+</script>
