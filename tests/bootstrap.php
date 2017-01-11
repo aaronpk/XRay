753407c904324d649e1bc02e8c0d514600abf90f
Aaron Parecki <aaron@parecki.com>
2017-01-11 10:06:45 -0800

    set default config for test suite

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,13 @@
 <?php
-namespace p3k;
 const TESTING = true;
 require __DIR__ . '/../vendor/autoload.php';
+
+// Load config file if present, otherwise use default
+if(file_exists(dirname(__FILE__).'/../config.php')) {
+  require dirname(__FILE__).'/../config.php';
+} else {
+  class Config {
+    public static $cache = false;
+  }
+}
+
