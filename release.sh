898281bbc75be4cd8e2edcc354ab197ee5e8005d
Aaron Parecki <aaron@parecki.com>
2017-01-11 09:55:42 -0800

    update composer, adds release script
    release script builds a zip file for releases

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+release_dir=../XRay-release
+
+current=`pwd`
+
+rsync -ap --delete controllers $release_dir/
+rsync -ap --delete lib $release_dir/
+rsync -ap --delete public $release_dir/
+rsync -ap --delete views $release_dir/
+rsync -ap --delete --exclude=.git vendor $release_dir/
+cp README.md $release_dir/
+cp LICENSE.txt $release_dir/
+cp index.php $release_dir/
+cp .htaccess $release_dir/
+cp controllers/.htaccess $release_dir/vendor/
+
+cd $release_dir
+zip -r xray.zip .
+
+cd $current
