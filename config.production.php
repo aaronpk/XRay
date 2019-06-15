2fe251d3082b44fc7d9dfe496329ecbd60d3bae5
Aaron Parecki <aaron@parecki.com>
2019-06-15 14:34:57 -0700

    update to new base URL

--- config.production.php
+++ config.production.php
@@ -1,6 +1,6 @@
 <?php
 class Config {
-  public static $base = 'https://xray.p3k.io';
+  public static $base = 'https://xray.p3k.app';
   public static $cache = true;
   public static $admins = [
     'https://aaronparecki.com/'
